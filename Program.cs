010afb24f9f1255db595f2528c48cd9e96ac7e38
Admin <Admin@WIN-0QFN66U68EP>
2024-12-23 18:26:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст:");
+        string input = Console.ReadLine();
+
+        string result = ReplaceWords(input);
+
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static string ReplaceWords(string input)
+    {
+        return input.Replace("one", "1")
+                    .Replace("two", "2")
+                    .Replace("three", "3")
+                    .Replace("four", "4")
+                    .Replace("five", "5");
+    }
+}
